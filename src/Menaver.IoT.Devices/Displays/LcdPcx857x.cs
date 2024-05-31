5d5036309455cf52909b8673572ef7c59df8285a
Dima Kileyko <dmitrykileyko@coherentsolutions.com>
2024-05-31 15:38:11 +0200

    implement keypad & LCD

---
+++ src/Menaver.IoT.Devices/Displays/LcdPcx857x.cs
@@ -0,0 +1,257 @@
+﻿using System.Device.Gpio;
+using System.Device.I2c;
+using Iot.Device.CharacterLcd;
+using Iot.Device.Pcx857x;
+
+namespace Menaver.IoT.Devices.Displays;
+
+/// <summary>
+///     Abstraction of interaction with LCD display module (PCX857X).
+///     Navigate to
+///     <see href="https://github.com/nanoframework/nanoframework.IoT.Device/blob/develop/devices/Pcx857x/README.md/">PCX857X</see>
+///     for more details about the module.
+/// </summary>
+public interface ILcdPcx857x
+{
+    Task WriteTextAsync(string text);
+    Task WriteTextAsync(string text, CancellationToken cancellationToken);
+    Task ClearAsync();
+    Task ClearAsync(CancellationToken cancellationToken);
+}
+
+/// <summary>
+///     Implementation of interaction with LCD display module (PCX857X).
+///     Navigate to
+///     <see href="https://github.com/nanoframework/nanoframework.IoT.Device/blob/develop/devices/Pcx857x/README.md/">PCX857X</see>
+///     for more details about the module.
+/// </summary>
+public class LcdPcx857x : ILcdPcx857x
+{
+    // For PCF8574T i2c addresses can be between 0x27 and 0x20 depending on bridged solder jumpers
+    // and for PCF8574AT i2c addresses can be between 0x3f and 0x38 depending on bridged solder jumpers
+    private static readonly int[] PossiblePorts = { 0x3F, 0x3E, 0x3D, 0x3C, 0x3B, 0x3A, 0x39, 0x38, 0x27 };
+    private readonly int[] _dataPins;
+
+    private readonly Pcx857xDevice _device;
+    private readonly int _deviceActualPortAddress;
+    private readonly int _displayColumnCount;
+    private readonly int _displayRowCount;
+    private readonly int _sclPin;
+    private readonly int _sdaPin;
+
+    public LcdPcx857x(
+        Pcx857xDevice device,
+        int sdaPin,
+        int sclPin,
+        int displayRowCount,
+        int displayColumnCount,
+        int[] dataPins = null!)
+    {
+        _device = device;
+
+        foreach (var possiblePort in PossiblePorts)
+        {
+            if (CheckDevice(_device, possiblePort, sdaPin, sclPin, dataPins))
+            {
+                _deviceActualPortAddress = possiblePort;
+                _sdaPin = sdaPin;
+                _sclPin = sclPin;
+                _dataPins = dataPins;
+                _displayRowCount = displayRowCount;
+                _displayColumnCount = displayColumnCount;
+
+                break;
+            }
+
+            throw new ArgumentException(
+                "LCD PCF8574 isn't accessible. Please, make sure device is injected and enabled.");
+        }
+    }
+
+    public LcdPcx857x(
+        Pcx857xDevice device,
+        int portAddressAddress,
+        int sdaPin,
+        int sclPin,
+        int displayRowCount,
+        int displayColumnCount,
+        int[] dataPins = null!)
+    {
+        _device = device;
+
+        if (!CheckDevice(_device, portAddressAddress, sdaPin, sclPin, dataPins))
+        {
+            throw new ArgumentException(
+                "LCD PCF8574 isn't accessible by the port and pins specified. Please, try another ones.");
+        }
+
+        _deviceActualPortAddress = portAddressAddress;
+        _sdaPin = sdaPin;
+        _sclPin = sclPin;
+        _dataPins = dataPins;
+        _displayRowCount = displayRowCount;
+        _displayColumnCount = displayColumnCount;
+    }
+
+    public Task WriteTextAsync(string text)
+    {
+        return WriteTextAsync(text, CancellationToken.None);
+    }
+
+    public Task WriteTextAsync(string text, CancellationToken cancellationToken)
+    {
+        var task = new Task(() => WriteText(text));
+
+        task.Start();
+        Task.WaitAll(new[] { task }, cancellationToken);
+
+        return task;
+    }
+
+    public Task ClearAsync()
+    {
+        return ClearAsync(CancellationToken.None);
+    }
+
+    public Task ClearAsync(CancellationToken cancellationToken)
+    {
+        var task = new Task(ClearScreen);
+
+        task.Start();
+        Task.WaitAll(new[] { task }, cancellationToken);
+
+        return task;
+    }
+
+    private void WriteText(string text)
+    {
+        var connection = new I2cConnectionSettings(busId: 1, deviceAddress: _deviceActualPortAddress);
+        using var module = I2cDevice.Create(connection);
+        using var driver = BuildDriver(_device, module);
+        using var controller = new GpioController(PinNumberingScheme.Logical, driver);
+
+        using var lcd = new Lcd1602(
+            registerSelectPin: 0,
+            enablePin: _sdaPin,
+            backlightPin: _sclPin,
+            dataPins: _dataPins ?? new[] { 4, 5, 6, 7 },
+            controller: controller);
+
+        lcd.Clear();
+
+        string[] lines;
+        if (text.Contains(Environment.NewLine))
+        {
+            lines = text.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+        }
+        else
+        {
+            if (text.Length <= _displayColumnCount)
+            {
+                lines = new[] { text };
+            }
+            else
+            {
+                // split by length to fix the display size
+                lines = new string[_displayRowCount];
+                for (var i = 0; i < _displayRowCount; i++)
+                {
+                    if (_displayColumnCount * i + _displayColumnCount >= text.Length)
+                    {
+                        lines[i] = text.Substring(_displayColumnCount * i, text.Length - _displayColumnCount);
+                    }
+                    else
+                    {
+                        lines[i] = text.Substring(_displayColumnCount * i, _displayColumnCount);
+                    }
+                }
+            }
+        }
+
+        for (var i = 0; i < lines.Length; i++)
+        {
+            if (i == _displayRowCount)
+            {
+                // to prevent falling out of bound
+                return;
+            }
+
+            try
+            {
+                lcd.SetCursorPosition(0, i);
+                lcd.Write(lines[i]);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                // this occurs when we're out of bound
+                break;
+            }
+        }
+    }
+
+    private void ClearScreen()
+    {
+        var connection = new I2cConnectionSettings(busId: 1, deviceAddress: _deviceActualPortAddress);
+        using var module = I2cDevice.Create(connection);
+        using var driver = BuildDriver(_device, module);
+        using var controller = new GpioController(PinNumberingScheme.Logical, driver);
+
+        using var lcd = new Lcd1602(
+            registerSelectPin: 0,
+            enablePin: _sdaPin,
+            backlightPin: _sclPin,
+            dataPins: _dataPins ?? new[] { 4, 5, 6, 7 },
+            controller: controller);
+
+        lcd.Clear();
+    }
+
+    private static bool CheckDevice(Pcx857xDevice device, int portAddress, int sdaPin, int sclPin, int[] dataPins)
+    {
+        try
+        {
+            var connection = new I2cConnectionSettings(busId: 1, deviceAddress: portAddress);
+            using var module = I2cDevice.Create(connection);
+            using var driver = BuildDriver(device, module);
+            using var controller = new GpioController(PinNumberingScheme.Logical, driver);
+
+            using var lcd = new Lcd1602(
+                registerSelectPin: 0,
+                enablePin: sdaPin,
+                backlightPin: sclPin,
+                dataPins: dataPins ?? new[] { 4, 5, 6, 7 },
+                controller: controller);
+
+            lcd.Clear();
+            lcd.SetCursorPosition(0, 0);
+            lcd.Write("test");
+            lcd.Clear();
+
+            return true;
+        }
+        catch (IOException)
+        {
+            return false;
+        }
+    }
+
+    private static Pcx857x BuildDriver(Pcx857xDevice device, I2cDevice module)
+    {
+        return device switch
+        {
+            Pcx857xDevice.Pcf8574 => new Pcf8574(module),
+            Pcx857xDevice.Pcf8575 => new Pcf8575(module),
+            Pcx857xDevice.Pca8574 => new Pca8574(module),
+            Pcx857xDevice.Pca8575 => new Pca8575(module),
+            _ => throw new ArgumentOutOfRangeException(nameof(device), device, null)
+        };
+    }
+}
+
+public enum Pcx857xDevice : byte
+{
+    Pcf8574 = 0,
+    Pcf8575 = 1,
+    Pca8574 = 2,
+    Pca8575 = 3
+}
---
+++ src/Menaver.IoT.Devices/Keyboards/MatrixKeyboard.cs
@@ -0,0 +1,173 @@
+﻿using System.Device.Gpio;
+using System.Timers;
+using Iot.Device.KeyMatrix;
+using Timer = System.Timers.Timer;
+
+namespace Menaver.IoT.Devices.Keyboards;
+
+/// <summary>
+///     Abstraction of interaction with matrix keyboard circuit.
+///     Navigate to <see href="https://en.wikipedia.org/wiki/Keyboard_matrix_circuit">Keyboard matrix circuit</see>
+///     for more details about the module.
+/// </summary>
+public interface IMatrixKeyboard : IDisposable
+{
+    Task<char> ReadKeyAsync();
+    Task<char> ReadKeyAsync(CancellationToken cancellationToken);
+}
+
+/// <summary>
+///     Implementation of interaction with matrix keyboard circuit.
+///     Navigate to <see href="https://en.wikipedia.org/wiki/Keyboard_matrix_circuit">Keyboard matrix circuit</see>
+///     for more details about the module.
+/// </summary>
+public class MatrixKeyboard : IMatrixKeyboard
+{
+    private readonly char[,] _keyMap;
+    private readonly KeyMatrix _keypad;
+    private readonly Timer _pressListeningTimer;
+
+    private CancellationTokenSource _pressListeningCancellationTokenSource;
+
+    public MatrixKeyboard(
+        int[] inputPins,
+        int[] outputPins,
+        char[,] keyMap,
+        int scanIntervalInMilliseconds = 10)
+    {
+        if (keyMap.Rank != 2)
+        {
+            throw new ArgumentException("The key map is supposed to be 2-dimensional array.");
+        }
+
+        if (keyMap.GetLength(0) != inputPins.Length)
+        {
+            throw new ArgumentException("Input pin count does not match the keymap row count.");
+        }
+
+        if (keyMap.GetLength(1) != outputPins.Length)
+        {
+            throw new ArgumentException("Output pin count does not match the keymap column count.");
+        }
+
+        _keyMap = keyMap;
+
+        _keypad = new KeyMatrix(outputPins, inputPins, TimeSpan.FromMilliseconds(scanIntervalInMilliseconds));
+
+        _pressListeningTimer = new Timer(scanIntervalInMilliseconds * (outputPins.Length + 3));
+        _pressListeningTimer.Elapsed += PressListeningTimerOnElapsed;
+    }
+
+    public async Task<char> ReadKeyAsync()
+    {
+        return await ReadKeyAsync(CancellationToken.None);
+    }
+
+    public async Task<char> ReadKeyAsync(CancellationToken cancellationToken)
+    {
+        while (true)
+        {
+            var key = await ReadKeyRaisedAsync(cancellationToken);
+
+            if (key != null)
+            {
+                _pressListeningCancellationTokenSource = new CancellationTokenSource();
+                _pressListeningTimer.Enabled = true;
+
+                var rowIndex = key.Input;
+                var columnIndex = await DetermineColumnIndexAsync(key.Output,
+                    _pressListeningCancellationTokenSource.Token);
+
+                if (columnIndex != null)
+                {
+                    return _keyMap[rowIndex, columnIndex.Value];
+                }
+            }
+        }
+    }
+
+    private async Task<int?> DetermineColumnIndexAsync(int columnIndexToExclude, CancellationToken cancellationToken)
+    {
+        var columnsCount = _keyMap.GetLength(1);
+        var columns = new List<int>(columnsCount - 1);
+        for (var i = 0; i < columnsCount; i++)
+        {
+            if (i == columnIndexToExclude)
+            {
+                continue; // exclude the column that's raised above 
+            }
+
+            columns.Add(i);
+        }
+
+        do
+        {
+            try
+            {
+                var key = await ReadKeyRaisedAsync(cancellationToken);
+
+                if (key != null)
+                {
+                    var columnRaised = key.Output;
+                    columns.Remove(columnRaised); // exclude the column that was raised
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                break;
+            }
+        } while (columns.Any());
+
+        // return the one that's left (remained not triggered, i.g. pressed)
+        return columns.Any() ? columns.First() : null;
+    }
+
+    private async Task<KeyMatrixEvent?> ReadKeyRaisedAsync(CancellationToken cancellationToken)
+    {
+        var task = new Task<KeyMatrixEvent>(() => _keypad.ReadKey());
+
+        task.Start();
+        Task.WaitAll(new Task[] { task }, cancellationToken);
+
+        var key = await task;
+
+        return key?.EventType.HasFlag(PinEventTypes.Rising) == true ? key : null;
+    }
+
+    private void PressListeningTimerOnElapsed(object sender, ElapsedEventArgs e)
+    {
+        _pressListeningTimer.Enabled = false;
+        _pressListeningCancellationTokenSource.Cancel();
+    }
+
+    #region IDisposable
+
+    ~MatrixKeyboard()
+    {
+        Dispose(false);
+    }
+
+    private bool _disposedValue;
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposedValue)
+        {
+            if (disposing)
+            {
+                _keypad.StopListeningKeyEvent();
+                _keypad.Dispose();
+            }
+
+            _disposedValue = true;
+        }
+    }
+
+    #endregion
+}
